a74e3b3b424d461c90497866cd5be93ac1a7aa17
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-30 16:36:16 -0700

    so I have added the inquire package and ive included all the packages I need for the application in the index.js, also made the first question inside the questions array

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your Project Name?'
+    },
+    {
+
+    },
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
