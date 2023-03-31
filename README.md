65bf3cca55bffda3c1d135376eaa4913ae6be0f7
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-30 18:02:16 -0700

    finished the generateMarkdown.js file

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# README-Generator
+# README Generator
+
+## Description
+
+In this section, provide a detailed
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+can understand what your project is all about.
+
+## Usage
+
+In this section, provide 
+
+[License](#license)
+
+## Contributing 
+
+Provide information about how others can contribute to your project. This could include information about submitting bug reports, suggesting new features, or contributing code.
+
+## Tests
+  
+Include information about any tests that you have run on your project. This could include details about the testing process, any tools or frameworks that you used, and the results of the tests.
+
+## Questions
+
+How to reach me? 
+
+Any further questions please contact me at ayalaarellanoanthony@gmal.com
+
+You can also check out my [GitHub profile](https://github.com/tonysm-10) for more projects.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'Mit') {
+    return `![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  }
+  else {
+    return ''; 
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Mit') {
+    return '[MIT](https://opensource.org/licenses/MIT)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'Mit') {
+    return '[License](#license)'
+  }
+  else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${licenseSection}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${licenseBadge}
+
+  ${licenseLink}
+
+  ## Contributing 
+
+  ${data.contributing}
+
+  ## Tests
+  
+  ${data.tests}
+
+  ## Questions
+
+  How to reach me? 
+
+  Any further questions please contact me at ${data.email}
+
+  You can also check out my [GitHub profile](https://github.com/${data.github}) for more projects.
 `;
 }
 
